655c43afd5f4b550bd43d02041833eabd743c959
AlexLoWCD <alexandre.lorion@wildcodeschool.fr>
2019-04-10 10:43:44 +0200

    init react pj

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Initiation à Netlify
+
+## Instructions
+
+* Forke le projet sur ton compte GitHub
+
+* Déploie le projet sur Netlify
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          <h1> Bienvenue sur Netlify !</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
